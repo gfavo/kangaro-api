6660452a5e06e3303fb3c8a287f8318278dfd7db
gfavo <gabriel.lfavaro@gmail.com>
2025-03-12 14:38:52 -0300

    Receiving calls from app and connecting to postgres

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,10 +1,32 @@
-import {Request, Response} from 'express';
+import { Request, Response } from "express";
+import User from "../models/user";
+import { NeonDbError } from "@neondatabase/serverless";
+require("dotenv").config();
+
+const { neon } = require("@neondatabase/serverless");
+
+const sql = neon(process.env.DATABASE_URL);
 
 export const getUsers = async (req: Request, res: Response) => {
-    try {
-        res.json('Worked!');
-    } catch(e) {
-        res.json('Didnt work!');
-    }
-    
+  try {
+    const result = await sql`SELECT version()`;
+    const { version } = result[0];
+    res.json("Worked! - " + version);
+  } catch (e) {
+    res.json("Didnt work!");
+  }
+};
+
+export const postUser = async (req: Request, res: Response) => {
+  try {
+    const data = req.body as User;
+    const result = await sql`insert into
+  "admins" ("id", "name", "password", "username")
+values
+  (default, ${data.name}, ${data.password}, ${data.username})`;
+    res.json("Worked!");
+  } catch (e) {
+    res.json((e as NeonDbError).detail);
+    console.log(e);
+  }
 };
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getUsers } from '../controllers/userController';
+import { getUsers, postUser } from '../controllers/userController';
 
 const router = express.Router();
 
 router.get('/', getUsers);
+
+router.post('/', postUser);
 
 export default router;
